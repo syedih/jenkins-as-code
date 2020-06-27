24403ac1a6688f9ffdb3defec34e0aac3e601c5b
masoodmjan <mjan@mazataz.com>
2020-06-27 12:14:14 +0100

    first test

---
+++ buil-push-deploy-ebs.groovy
@@ -0,0 +1,58 @@
+pipeline{
+    agent any
+    tools {
+
+    }
+    options {
+        buildDiscarder(logRotator(numToKeepStr: '100', daysToKeepStr: '30'))
+        disableConcurrentBuilds()
+        timestamps()
+    }
+
+    parameters{
+        choice(name: 'EB_Action',  choices: ['deploy','create'], description: 'deploy : Deploys to existing environment <br> create :  Creates a new environment ')
+    }
+    stages{
+        stage("Pull Code From github"){
+            steps{
+                cleanWs()
+                dir('scheduler-scripts'){
+                    checkout([$class: 'GitSCM', branches: [[name: scripts_branch ]],
+                              userRemoteConfigs: [[
+                                                          credentialsId: 'github_devops',
+                                                          url: 'https://github.com/askmas/tutorials1' ]]
+                    ])
+                }
+
+            }
+        }
+
+        stage("Build Docker image"){
+            steps{
+                script{
+                    sh 'docker build -t masoodmjan/tutorials .'
+                }
+            }
+        }
+
+
+        stage("Push Image to Docker Hub"){
+            steps{
+                script{
+                    sh '  cat /my_pass.txt | docker login --username=masoodmjan --password-stdin '
+                    sh ' docker push masoodmjan/tutorials'
+                }
+            }
+        }
+
+        stage("Deploy To AWS ebs"){
+            steps{
+                script{
+                    sh   'cd ElasticBeanstalk\n' +
+                          '. ./BuildAndDeployDockerEBs.sh'
+
+                }
+            }
+        }
+    }
+}
